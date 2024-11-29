30d7f7decef20d82cb56ff72412375f586fa9c6c
Adarsh S <adarsh354@gmail.com>
2024-11-29 02:40:13 -0700

    Implemented file watcher and config file reader functionality

--- src/kafka_producer/kafka_topic_creator.py
+++ src/kafka_producer/kafka_topic_creator.py
@@ -1,36 +1,39 @@
 from kafka.admin import KafkaAdminClient, NewTopic
 from kafka import errors
+import json
 
-KAFKA_BROKER = "localhost:9092"
-topics = [
-    'Devices_Dim',
-    'logs_collections',
-    'Maint_Device_Data',
-    'Motion_Detect_Data',
-    'Temp_Hum_Sensor_Data'
-]
+
+CONFIG_PATH = "../../config/config.json"
 topic_list = []
 
+def read_config():
+    with open(CONFIG_PATH) as config_file:
+        config = json.load(config_file)
+        KAFKA_BROKER = config["KAFKA_PRODUCER"]["KAFKA_HOST"]
+        topics = config["KAFKA_PRODUCER"]["topic_list"]
+        print("Config file read successfully")
+    return KAFKA_BROKER, topics
 
 #Function to create topic
 def create_topic(admin_client, topics):
+    print("in create_TOPIC")
+    print(topics)
     for topic_name in topics:
+        print(topic_name)
         topic_list.append(NewTopic(topic_name, num_partitions=1, replication_factor=1))
         print("****Topic: " + topic_name)
     try:
-        admin_client.create_topics(new_topics=topic_list, validate_only=False)
+        admin_client.create_topics(topic_list)
     except errors.TopicAlreadyExistsError:
         print("*Topics exist already*")
     print("Topics list completed")
-    
 
-
-
+KAFKA_BROKER, TOPICS = read_config()
 admin_client = KafkaAdminClient(
     bootstrap_servers = KAFKA_BROKER, 
     client_id='test'
 )
-create_topic(admin_client, topics)
+create_topic(admin_client, TOPICS)
     
 
 print("New topic list:")
