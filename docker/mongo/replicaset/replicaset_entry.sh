bd5d34075fc4b0a7d30f8549d66bfeecfb0cbf73
nishalcr <37110101+nishalcr@users.noreply.github.com>
2024-11-30 14:01:41 -0700

    Modified: Updated with the changes to mongo-cluster

--- docker/mongo/replicaset/replicaset_entry.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-exec /usr/local/bin/replicaset.sh &
-exec /usr/local/bin/docker-entrypoint.sh "$@"
--- docker/mongo/router/router_entry.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-exec /usr/local/bin/router.sh &
-exec /usr/local/bin/docker-entrypoint.sh "$@"
